490583745d5150b66ec278b3099a49d2109b5724
= <=>
2019-10-04 15:22:43 +0200

    added script to run webpack project

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const path = require("path");
 // configuration webpack will going to check
 module.exports = {
     mode: "development", // or production
-    entry: path.resolve(__dirname, 'src', 'app'), // is the firdt file of our app
+    entry: path.resolve(__dirname, 'src', 'app'), // is the first file of our app
     output: { // what our application is going to become 
-        path: path.resolve(__diranme, 'dist'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/'
     }
