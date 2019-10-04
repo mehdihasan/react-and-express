72849d7550164c36855941a6e640a66dfce39daa
= <=>
2019-10-04 15:35:35 +0200

    add config / setup for filetypes to be processed by webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,24 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/'
+    }, 
+    resolve: {
+        // these are the filetypes we want to webpack to process
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        // if we want to enable react router 
+        historyApiFallback: true
+    },
+    // how we want our app to be compiled
+    module: {
+        // if you found any file ends with .js or .jsx, 
+        // hey webpack, you should use babel-loader to compile the file
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader'
+        }]
     }
+
+    
 }
